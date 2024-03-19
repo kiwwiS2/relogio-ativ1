a5be7d303e1cbdd6aa7a936c6bedf50dfceab091
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-03-19 09:57:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const botoes = document. querySelectorAll("botao");
-for(let i=0;i<botoes.length;i++) {
+for(let i=0;i<botoes.length;i++) 
+   botoes(i).onclik = function (){
+ 
+    for (let j=0;botoes.length;j++) {
+        botoes[j].classList.remove("ativo");
+    }
+   botoes(1).classList.add("ativo");
 
 }
