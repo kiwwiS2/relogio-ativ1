62b7db02847b107e9830af02717b73092e1d3fd1
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-04-09 12:17:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ const textos = document.querySelectorAll(".abas-conteudo");
    textos[i].classList.add("ativo");
  }
 }
+ const contadores = document.querySelectorAll(".contador");
+
+ const tempoObjetivo1 = new Dare ("2024-05-03T00:00:00");
+ const tempoObjetivo2 = new Dare ("2024-12-05T00:00:00");
+ const tempoObjetivo3 = new Dare ("2024-12-30T00:00:00");
+ const tempoObjetivo4 = new Dare ("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+ 
+function calculaTempo(tempoObjetivo1) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+
+
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
+}
