cc7465a7ebbb5d8fb068d8f8f53abc8902ffd458
kiwi <106825312+kiwwiS2@users.noreply.github.com>
2024-04-16 11:25:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,53 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".abas-conteudo");
+//laço de repetição
 
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function(){
 
- for(let i = 0 ; i < botoes.length; i++){ 
-   botoes[i].onclick = function (){
- 
-    for (let j = 0; j < botoes.length; j++) {
-        botoes[j].classList.remove("ativo");
-        textos[j].classList.remove("ativo");
+        for (let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
-   botoes[i].classList.add("ativo");
-   textos[i].classList.add("ativo");
- }
 }
- const contadores = document.querySelectorAll(".contador");
+const contadores = document.querySelectorAll(".contador");
 
- const tempoObjetivo1 = new Dare ("2024-05-03T00:00:00");
- const tempoObjetivo2 = new Dare ("2024-12-05T00:00:00");
- const tempoObjetivo3 = new Dare ("2024-12-30T00:00:00");
- const tempoObjetivo4 = new Dare ("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-26T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-24T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-19T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-12T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
- 
-function calculaTempo(tempoObjetivo1) {
-  let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
+function calculatempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual
+    let segundos = math.floor(tempoFinal/1000)
+    let minutos = math.floor(segundos/60)
+    let horas = math.floor(minutos/60)
+    let dias = math.floor(horas/24)
+
+    segundos %=60
+    minuto %=60
+    horas %=24
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 
 
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24);
-
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-
-return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
 }
+contadores[0].textContent = calculatempo(tempoObjetivo1)
+
+function atualizacronometro(){
+    for(let i=0;i<contadores.length;i++){
+        contadores[i].textContent = calculatempo(tempos[i])
+    }
+}
+function comecacronometro(){
+    atualizacronometro();
+    setInterval(atualizacronometro)
+}
+comecacronometro();
